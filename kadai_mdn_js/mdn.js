2098bdd6730f3846cafd30057ff18a276c4bf31d
Yu <31719336+Yu62ballena@users.noreply.github.com>
2024-01-06 13:37:51 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const date = new Date(2023, 8, 22);
-const [year, month, day] = [
-  date.getFullYear(),
-  date.getMonth(),
-  date.getDate(),
-];
-
-const fullDate = `${year}年${month}月${day}日`;
-console.log(fullDate);
